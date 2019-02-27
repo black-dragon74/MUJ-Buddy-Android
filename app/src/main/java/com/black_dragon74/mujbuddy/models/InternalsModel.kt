24a14896a8ec8b55caf353e46f5e448c6d0fbd9c
black.dragon74 <nickk.2974@gmail.com>
2019-02-27 20:49:35 +0530

    Sync with the API changes
    No need to be happy. Just fixed what needed fixing. Internal marks are now parsed dynamically.
    
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- app/src/main/java/com/black_dragon74/mujbuddy/models/InternalsModel.kt
+++ app/src/main/java/com/black_dragon74/mujbuddy/models/InternalsModel.kt
@@ -1,12 +1,12 @@
 package com.black_dragon74.mujbuddy.models
 
 class InternalsModel(
-    val index: String,
-    val subject: String,
-    val cws: String,
-    val prs: String,
-    val mte_1: String,
-    val mte_2: String,
-    val total: String,
-    val re_session: String?
+    val sno: String,
+    val subject_codes: String,
+    val cws: String?,
+    val prs: String?,
+    val mte_1: String?,
+    val mte_2: String?,
+    val total: String?,
+    val resession: String?
 )
