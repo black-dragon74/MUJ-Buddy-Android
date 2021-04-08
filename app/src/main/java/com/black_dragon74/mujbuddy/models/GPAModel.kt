4e71697e6d4af1d1eb01ccc7b4ef990894b60451
black.dragon74 <nickk.2974@gmail.com>
2021-04-08 20:59:31 +0530

    Update GPAModel to use custom coding keys
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- app/src/main/java/com/black_dragon74/mujbuddy/models/GPAModel.kt
+++ app/src/main/java/com/black_dragon74/mujbuddy/models/GPAModel.kt
@@ -1,13 +1,29 @@
 package com.black_dragon74.mujbuddy.models
 
+import com.google.gson.annotations.SerializedName
+
 data class GPAModel(
-    val success: Boolean,
-    val semester_1: String?,
-    val semester_2: String?,
-    val semester_3: String?,
-    val semester_4: String?,
-    val semester_5: String?,
-    val semester_6: String?,
-    val semester_7: String?,
-    val semester_8: String?
+        @SerializedName("gpa_semester_i")
+        val semester_1: String?,
+
+        @SerializedName("gpa_semester_ii")
+        val semester_2: String?,
+
+        @SerializedName("gpa_semester_iii")
+        val semester_3: String?,
+
+        @SerializedName("gpa_semester_iv")
+        val semester_4: String?,
+
+        @SerializedName("gpa_semester_v")
+        val semester_5: String?,
+
+        @SerializedName("gpa_semester_vi")
+        val semester_6: String?,
+
+        @SerializedName("gpa_semester_vii")
+        val semester_7: String?,
+
+        @SerializedName("gpa_semester_viii")
+        val semester_8: String?
 )
